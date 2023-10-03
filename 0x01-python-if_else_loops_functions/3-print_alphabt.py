86f16adda063d1963c54deaded4571e43882d1d9
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-03 21:06:34 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 start_Ascii = 97
 end_Ascii = 122
-for i in range(start_Ascii, end_Ascii):
+for i in range(start_Ascii, end_Ascii+1):
     if i not in [113, 101]:
         print('{}'.format(chr(i)), end="")
