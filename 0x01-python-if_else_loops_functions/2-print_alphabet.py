ed829cf169a02a704375ee71215dd1ae1172b7e0
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-03 19:35:13 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 start_ascii = 97
 end_ascii = 122
-for i in range(start_ascii, end_ascii):
+for i in range(start_ascii, end_ascii+1):
     txt = chr(i)
     print(txt.format(), end="")
