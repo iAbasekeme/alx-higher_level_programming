af7b20597830962b7fd1402c6d6396f82e5b34cd
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-04 18:00:34 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("{:s}".format('Fizzbuzz'), end=" ")
+            print("FizzBuzz" end=" ")
         elif i % 3 == 0:
-            print("{:s}".format('Fizz'), end=" ")
+            print("Fizz", end=" ")
         elif i % 5 == 0:
-            print("{:s}".format('Buzz'), end=" ")
+            print("Buzz", end=" ")
         else:
-            print("{:d}".format(i), end=" ")
+            print("{}".format(i), end=" ")
