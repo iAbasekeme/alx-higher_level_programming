c50d8fb09d008d0f4383246dc43ed0f4e902ff0b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-05 16:26:45 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,5 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    add(a, b)
-    print(f"{a} + {b} = {a+b}")
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
