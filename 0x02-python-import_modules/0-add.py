ae4906b81e15bc641a6d4ff857bf9b77565449c3
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-05 16:04:03 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-add(a, b)
-print(f"{a} + {b} = {a+b}")
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    add(a, b)
+    print(f"{a} + {b} = {a+b}")
