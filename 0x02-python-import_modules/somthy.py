9f976704ce98e8fce517efa0b7f3b19f81033943
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-06 18:56:57 +0100

    Delete somthy.py

--- 0x02-python-import_modules/somthy.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-module_list = dir(hidden_4)
-for i in module_list:
-    if not i.startswith('__'):
-        print("{}".format(i))
