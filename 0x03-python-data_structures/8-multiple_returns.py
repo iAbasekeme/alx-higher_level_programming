37418e962f3e97f2df9665e6a86dc629c715e17d
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-09 16:04:20 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
+    if sentence == 0:
         sentence[0] = None
     else:
         return (len(sentence), sentence[0])
