47a6c8fd107e77b00ad8e62d3b7d68854ee91a3a
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-09 13:31:01 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        return None
     for row in matrix:
         for num in row:
             print("{:d}".format(num), end=" ")
