1b6b44279a9af4331266f1bf74c997922c75d4f2
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-11 18:03:38 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
-        return None
-    dupl_list = list(my_list)
-
-    new_list = [replace if x == search else x for x in dupl_list]
+    new_list = [replace if x == search else x for x in my_list]
     return new_list
