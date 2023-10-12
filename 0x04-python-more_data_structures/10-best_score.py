b1014a2ad311705913b012ba41e4d17bcca1cfff
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-12 19:53:45 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
         # this is possible because max operates on values
         max_values = max(a_dictionary.keys())
-    return max_values
+    if max_values is None:
+        return None
+    else:
+        return max_values
