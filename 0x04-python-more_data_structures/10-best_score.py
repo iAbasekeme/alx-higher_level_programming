1cd8d0c0717e478134662960a36ee3bd89d59b00
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-24 11:14:35 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,4 +6,10 @@ def best_score(a_dictionary):
         # this is possible because max operates on values
         # for each key key=a_dictionary.get is used to extract the associated value from the dictionary
         max_values = max(a_dictionary, key=a_dictionary.get)
-    return max_values
+    try:
+        if max_values is None:
+            return None
+    except:
+        return None
+    else:
+        return max_values
