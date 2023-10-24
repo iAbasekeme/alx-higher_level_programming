b5ce4ccf65ff2583d6f9d9ce956b8b55ba69183b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-24 17:44:03 +0100

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is a module that defines a module base on 0-square.py"""
+
+
+class Square:
+    """This is a class that initializes
+    an instance of the Square class"""
+
+    def __init__(self, size):
+        self._size = size
