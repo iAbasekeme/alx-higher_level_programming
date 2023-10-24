cb3c3cd3d16ba544efdaeec3000430f94f90b36b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-24 18:47:46 +0100

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is a module that defines a module base on 2-square"""
+
+
+class Square:
+    """This is a class that initializes
+    an instance of the Square class and raises errors
+    if the size is 0 or size is not int"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """This method returns the current square data"""
+        return self.__size * self.__size
