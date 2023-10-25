faaea170d7dcc73f5340c6472b761af583b13d11
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-25 14:31:34 +0100

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+class Square:
+    """This is a class that initializes
+    an instance of the Square class and raises errors
+    if the size is 0 or size is not int"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """This getter method returns the current data"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """This is a setter method"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """This instance method return the current square method"""
+        print(self.__size ** 2)
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        else:
+            for _ in range(self.size):
+                # this prints (#) self.size times for each run
+                print("#"*self.size)
