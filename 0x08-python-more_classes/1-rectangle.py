131b40b5770d23fa00bf66e630ab577d22967200
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-30 19:02:16 +0100

    Create 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module creates a new Rectangle class
+"""
+
+
+class Rectangle:
+    """This is a rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """This getter method returns the Rectangle width data"""
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        """This is a setter method"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.width = value
+
+    @property
+    def height(self):
+        """This instance method return the current Rectangle height"""
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.height = value
