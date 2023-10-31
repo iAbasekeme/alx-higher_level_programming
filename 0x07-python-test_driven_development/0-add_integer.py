5d2321bc6b8e843ddb9dec9b451e7a6072d4f2ef
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-31 18:27:07 +0100

    Create 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module contains a functions that adds two integers
+"""
+
+
+def add_integer(a, b=98):
+    """This functions adds two integers
+
+    Args:
+        a (int or float): First Argument
+        b (int or float, optional): second Argument. Defaults to 98.
+
+    Raises:
+    TypeError: a must be an integer
+    TypeError: b must be an integer
+
+    Returns:
+    int: result of te addition
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    if isinstance(a, float):
+        int(a)
+    if isinstance(b, float):
+        int(b)
+    return a + b
