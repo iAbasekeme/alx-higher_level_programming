ce3402504f9cfa427e8a127869f3f49f58feeb56
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-10-31 10:36:37 +0100

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,15 +16,6 @@ class Rectangle:
         """This getter method returns the Rectangle width data"""
         return self.__width
 
-    @width.setter
-    def width(self, value):
-        """This is a setter method"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
     @property
     def height(self):
         """This instance method return the current Rectangle height"""
@@ -33,7 +24,16 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise Exception("height must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """This is a setter method"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
