53572de1ab0bd1f646936b3c89a9ae544eda9a54
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-03 16:00:23 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -13,7 +13,10 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: first_name must be a string or last_name must be a string
     """
-    if not (isinstance(first_name, str) or (isinstance(last_name, str))):
+    if not (isinstance(first_name, str)):
+        raise TypeError(
+            'first_name must be a string or last_name must be a string')
+    if not (isinstance(last_name, str)):
         raise TypeError(
             'first_name must be a string or last_name must be a string')
     print("My name is {:s} {:s}".format(first_name, last_name))
