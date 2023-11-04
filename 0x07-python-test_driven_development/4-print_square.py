253aa31a70cde60b8e0be523643d5d02af2bc3d9
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-04 11:26:14 +0100

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -19,7 +19,7 @@ def print_square(size):
     if size < 0:
         raise ValueError('size must be >= 0')
 
-    if size is float and size < 0:
+    if isinstance(size, float) and size < 0:
         raise TypeError('size must be an integer')
 
     for _ in range(size):
