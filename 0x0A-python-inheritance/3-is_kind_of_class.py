120be166641e3bfd7ff88fdd441f5d84c274dc0c
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-06 17:05:24 +0100

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""A function that returns True if the object is an instance of 
-or if the object is an instance of a class that inherited 
+"""A function that returns True if the object is an instance of
+or if the object is an instance of a class that inherited
 from, the specified class ; otherwise False.
 """
 
