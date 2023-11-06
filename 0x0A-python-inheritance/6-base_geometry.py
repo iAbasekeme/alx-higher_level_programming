b1acd31f21895b67d3940038918d10c66138729c
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-06 17:51:13 +0100

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is amodule that holds a new class
+"""
+
+
+class BaseGeometry:
+    """This ia an empty class name BaseGeometry
+    """
+
+    def area(self):
+        raise Exception('area() is not implemented')
