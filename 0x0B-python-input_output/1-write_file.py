a164c9f92aee05281bc4ae97683c2852f0f7390a
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-07 11:58:36 +0100

    Create 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+"""This module hold sa function that wrotes to a file
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes to a file
+
+    Args:
+        filename (str, optional): file to write to. Defaults to "".
+        text (str, optional): Text to be written. Defaults to "".
+    """
+    with open('filename.txt', 'w') as file:
+        content = file.write(text)
+        return content
