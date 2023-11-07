0574065b8576b162140a0b524db3982dfcbcf67c
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-07 12:55:26 +0100

    Create 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+"""This module holds a function that
+returns obj represented by JSON
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns the an obj from JSON
+
+    Args:
+        my_str (string): String to be converted 
+    """
+    return json.loads(my_str)
