3c62caa2d71721cd8fcf4ad0f5eb41df28454566
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-07 12:48:32 +0100

    Create 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+"""This module holds a function that returns the json of an object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """This function returns the json rep of an object
+
+    Args:
+        my_obj (_type_): Any
+    """
+    return json.dumps(my_obj)
