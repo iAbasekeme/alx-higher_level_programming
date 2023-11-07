aabd3bacbcff9fce3625144a11eb5785f331e0a5
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-07 12:31:35 +0100

    Create 2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+"""This module holds a functio that appends to a file
+"""
+
+
+def append_write(filename="", text=""):
+    """A function that appends to a file
+
+    Args:
+        filename (str, optional): Name of the file. Defaults to "".
+        text (str, optional): Appendant. Defaults to "".
+    """
+    with open(filename, 'a') as file:
+        content = file.write(text)
+        return content
