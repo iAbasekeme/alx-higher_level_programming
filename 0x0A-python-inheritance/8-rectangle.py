a70be603fcad62fcb95d13e74f1849b93c59864e
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-09 00:28:07 +0100

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """This is a module that holds a new class
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
