1d8d075dc63764a4dc9c1ef2d5e8e73549551246
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-09 15:12:09 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,10 +6,12 @@ import sys
 save_to_json = __import__('5-save_to_json_file').save_to_json_file
 load_from_json = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv[1:]
 
-existing_json = load_from_json("add_item.json") or []
+def additems():
+    args = sys.argv[1:]
 
-new_items = existing_json + args
+    existing_json = load_from_json("add_item.json") or []
 
-save_to_json(new_items, "add_item.json")
+    new_items = existing_json + args
+
+    save_to_json(new_items, "add_item.json")
