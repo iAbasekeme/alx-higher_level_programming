6981701aee4314d1b323d842bb759353b64c470c
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-09 01:19:10 +0100

    Create 9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""full rectangle
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A subclass of a superclass
+
+    Args:
+        BaseGeometry (class): A class that validates it's attributes
+    """
+
+    def __init__(self, width, height):
+        """A constructor function that validates a height
+        and width of a rectangle
+
+        Args:
+            width (int): Width
+            height (int): Height
+        """
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+        def __str__(self):
+            """custom str method for print the rectangle"""
+            return "[Rectangle]" + str(self.__width) + "/" + str(self.__height)
+
+        def area(self):
+            """computes area of the retangle"""
+            return self.__height * self.__width
