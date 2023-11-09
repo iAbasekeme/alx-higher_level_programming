b1f5384241fffe429ef36e39c2ae9120be99c1f3
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-09 14:58:10 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,15 +3,13 @@
 to a Python list, and then save them to a file
 """
 import sys
-
 save_to_json = __import__('5-save_to_json_file').save_to_json_file
 load_from_json = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv[1:]
 print(args)
 
-existing_json = []
-existing_json = load_from_json("add_item.json")
+existing_json = load_from_json("add_item.json") or []
 
 new_items = existing_json + args
 
