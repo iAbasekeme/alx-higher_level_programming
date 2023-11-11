3d4ce7f63146fe3421015823fe3774c5f880f25b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-11 11:18:36 +0100

    Create 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A module that holds class Student that defines a student by:
+"""
+
+
+class Student:
+    """A student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(items, str)
+                                           for items in attrs):
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        # print(type(json))
+        for key, value in json.items():
+            # print(json.items())
+            setattr(self, key, value)
