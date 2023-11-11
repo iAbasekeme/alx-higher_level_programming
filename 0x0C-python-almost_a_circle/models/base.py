6978f751aa33b4f17c66ffd2e1db72ef693c71ab
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-11 15:59:19 +0100

    Create base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A new module for a class Base
+"""
+
+
+class Base:
+    """A Base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """class constructor
+
+        Args:
+            id (class, optional): public instance . Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            self.id = Base.__nb_objects + 1
