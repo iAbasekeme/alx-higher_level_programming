5332e9bbc8eea3caa77a1bc120aab8df2acc9921
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-11 01:33:36 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,19 +5,14 @@ to a Python list, and then save them to a file
 import sys
 from os.path import exists
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json = __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
+if exists(filename):
+    updated_data = load_from_json_file(filename)
+else:
+    updated_data = []
+    for arg in sys.argv[1:]:
+        updated_data.append(arg)
 
-def additems():
-    """a script that adds all arguments
-    to a Python list, and then save them to a file
-    """
-    filename = "add_item.json"
-    if exists(filename):
-        updated_data = load_from_json(filename)
-    else:
-        updated_data = []
-        for arg in sys.argv[1:]:
-            updated_data.append(arg)
-
-    save_to_json_file(updated_data, filename)
+save_to_json_file(updated_data, filename)
