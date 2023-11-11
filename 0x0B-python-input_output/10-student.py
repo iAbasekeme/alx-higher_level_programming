4f41efc78107c9133d3365e8002365fe70fa02f9
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-11 08:12:52 +0100

    Create 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module that holds class Student that defines a student by:
+"""
+
+
+class Student:
+    """A student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(items, str) for items in attrs):
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attrs)}
+        else:
+            return self.__dict__
