490e2cbbc940c40a794b4380cc1bd5c993489e93
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-11 08:16:44 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,11 @@ from os.path import exists
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-if exists(filename):
-    updated_data = load_from_json_file(filename)
+if exists("add_item.json"):
+    updated_data = load_from_json_file("add_item.json")
 else:
     updated_data = []
     for arg in sys.argv[1:]:
         updated_data.append(arg)
 
-save_to_json_file(updated_data, filename)
+save_to_json_file(updated_data, "add_item.json")
