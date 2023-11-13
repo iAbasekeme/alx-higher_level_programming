37528004ab461883535c00cead94967f5c2ad67e
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-13 19:01:40 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,3 +11,40 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Contructor for square"""
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """A getter method for size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """A setter method
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """An update method to update attribute of a class
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+
+        if kwargs:
+            self.id = kwargs.get('id', self.id)
+            self.width = kwargs.get('size', self.width)
+            self.x = kwargs.get('x', self.x)
+            self.y = kwargs.get('y', self.y)
+
+    def __str__(self):
+        """A str method to print to stdout
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y} - {self.width}"
