0cbb0e11212a1eba7df19da6d1bab8b2bc755e96
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-13 15:29:11 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,10 +15,14 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """A getter method for width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """A setter method
+        """
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
@@ -27,10 +31,14 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """A getter method for height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """A setter method for height
+        """
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
@@ -39,10 +47,14 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """A getter method for x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """A setter method for x
+        """
         if not isinstance(value, int):
             raise TypeError('x must be an integer')
         if value < 0:
@@ -51,12 +63,55 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """A getter method for y
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """A setter method for y
+        """
         if not isinstance(value, int):
             raise TypeError('y must be an integer')
         if value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
+
+    def area(self):
+        """A public method for calculating area
+        """
+        return self.width * self.height
+
+    def display(self):
+        """A method that displays the Rectangle
+            instance with the character # and also by
+            taking care of x and y
+        """
+        for _ in range(self.y):
+            print("{}".format(''))
+        for _ in range(self.height):
+            c = self.width
+            print("{}".format(" " * self.x + '#' * c))
+
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+
+        if kwargs:
+            self.id = kwargs.get('id', self.id)
+            self.width = kwargs.get('width', self.width)
+            self.height = kwargs.get('height', self.height)
+            self.x = kwargs.get('x', self.x)
+            self.y = kwargs.get('y', self.y)
+
+    def __str__(self):
+        return f"{self.__class__.__name__} ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
