9e937bb39ead8897e148274099ccd1c30107b5fc
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-14 15:33:34 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A new module for a class Base
 """
+import json
 
 
 class Base:
@@ -19,3 +20,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string
+        representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
