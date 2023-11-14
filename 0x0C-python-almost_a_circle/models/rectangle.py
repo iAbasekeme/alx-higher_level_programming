3f6ff66fa13efd7c0ed5be44a4c90ad48a46d84a
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-14 15:21:04 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -128,5 +128,5 @@ class Rectangle(Base):
     def to_dictionary(self):
         """A method that returns the dict represention of attributes
         """
-        return {'x': self.id, 'y': self.y, 'id': self.id,
+        return {'x': self.x, 'y': self.y, 'id': self.id,
                 'height': self.height, 'width': self.width}
