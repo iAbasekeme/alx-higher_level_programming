d623a3d20634f30c4d718d510bf7446eea502d81
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-14 14:34:52 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,29 +10,6 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor for square"""
-        if not isinstance(width, int):
-            raise TypeError('width must be an integer')
-
-        if not isinstance(height, int):
-            raise TypeError('height must be an integer')
-
-        if not isinstance(x, int):
-            raise TypeError('x must be an integer')
-
-        if not isinstance(y, int):
-            raise TypeError('y must be an integer')
-
-        if width < 0:
-            raise ValueError('width must be > 0')
-
-        if height < 0:
-            raise ValueError('height must be > 0')
-
-        if x < 0:
-            raise ValueError('x must be >= 0')
-
-        if y < 0:
-            raise ValueError('y must be >= 0')
 
         super().__init__(id)
         self.width = width
