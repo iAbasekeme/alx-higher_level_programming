914d7496bb1195756287d3c12638cce9d4681fe8
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-11-14 11:19:50 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,6 +9,7 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor for square"""
         self.width = width
         self.height = height
         self.x = x
