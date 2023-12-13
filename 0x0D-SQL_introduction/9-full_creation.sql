b6117c82dadc42802dac9b6f7921fe8f9c6157a2
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-13 12:53:13 +0100

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- A script that creates a table second_table in the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table(
     id INT,
-    name VARCHAR(256)
+    name VARCHAR(256),
     score INT
 );
 
