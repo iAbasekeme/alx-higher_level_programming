fa986bf039ce3d3d12a2fb93d6b872984999bab8
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-14 23:52:15 +0100

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- A script that creates a user in our server
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd;
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
