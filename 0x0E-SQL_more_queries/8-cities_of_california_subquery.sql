2879ac35201dcf724127af19b70490fd4e706b83
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-15 09:39:17 +0100

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- A script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT * 
+SELECT id, name
 FROM cities 
 WHERE state_id = (SELECT id FROM states WHERE name = 'california' )
 ORDER BY cities.id ASC;
