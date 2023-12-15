aafa2a9aeefb3e65cb20c8b92217c660c690449c
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-15 08:59:22 +0100

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,13 +1,13 @@
 -- A script that creates the database hbtn_0d_usa and the table cities
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
-USE hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities
 (
     id INT AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    UNIQUE (id),
+    name VARCHAR(256) NOT NULL,    
     PRIMARY KEY (id),
+    UNIQUE (id),
     FOREIGN KEY(state_id) REFERENCES states(id)
 );
