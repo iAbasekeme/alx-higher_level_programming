a692d54f70fd16621a1aa7bcc659a62adc993842
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-15 10:31:30 +0100

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.id
+SELECT cities.id, cities.name, cities.states.id
 FROM cities
 ORDER BY cities.id ASC;
