f307bb0ac7b7ff53428e6fd07854797fa1f078e9
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-15 08:55:37 +0100

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,9 +4,10 @@ USE hbtn_0d_usa
 
 CREATE TABLE IF NOT EXISTS cities
 (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
+    UNIQUE (id),
     PRIMARY KEY (id),
     FOREIGN KEY(state_id) REFERENCES states(id)
 );
