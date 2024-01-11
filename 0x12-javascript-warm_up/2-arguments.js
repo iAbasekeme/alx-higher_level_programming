69637346d1e37b7afa468d1d78834bc9844f9461
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-11 22:53:31 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const process = require('process');
-if (process.argv == 1 || process.argv == 2){
+if (process.argv == 0 || process.argv == 1){
         console.log('No argument');
 }else if (process.argv == 3){
         console.log('Argument found');
