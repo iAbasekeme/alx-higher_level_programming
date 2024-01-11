42c93ece64c1e9d2891c8b1a45815a11611aed74
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-11 23:09:25 +0100

    fixed semistandard errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const process = require('process');
 if (process.argv.length === 1 || process.argv.length === 2) {
-    console.log('No argument');
-} else if (process.argv.length === 3){
-    console.log('Argument found');
+	console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
