624d024d0b22c9dc7184ad94251ea7a2c9ebc322
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-12 02:02:11 +0100

    Create 9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,20 @@
+// #!/usr/bin/node
+
+function add(a, b){
+  return a + b;
+}
+const process = require('process');
+const args = process.argv;
+
+if (parseInt(args[2]) && parseInt(args[3])) {
+  const num1 = parseInt(args[2]);
+  const num2 = parseInt(args[3]);
+
+  if (!isNaN(num1) && !isNaN(num2)) {
+  console.log(add(num1, num2));
+  } else {
+    console.log('NaN');
+  }
+} else {
+  console.log('NaN');
+}
