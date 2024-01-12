4992047a2e93368e6364e02984fcccd639774454
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-12 21:48:13 +0100

    Create 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+// #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+  rotate () {
+    if (this.width && this.height) {
+      [this.width, this.height] = [this.height, this.width];
+    }
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;
