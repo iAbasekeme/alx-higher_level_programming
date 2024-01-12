4c36091bb20c73621db9723ae575ed2127225294
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-12 14:18:51 +0100

    Create 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+// #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+
+    if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
+      return {};
+    }
+  }
+}
+module.exports = Rectangle;
