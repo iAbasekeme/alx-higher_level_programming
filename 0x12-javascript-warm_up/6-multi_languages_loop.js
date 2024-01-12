d8550e4ca85934f2c1dc8a36a9936d66cbb4273f
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-12 01:06:04 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const args = [];
-args.push('C is fun','Python is cool','JavaScript is amazing');
+args.push('C is fun', 'Python is cool', 'JavaScript is amazing');
 let i = 0;
 for (; i < args.length; i++) {
   console.log(args[i]);
