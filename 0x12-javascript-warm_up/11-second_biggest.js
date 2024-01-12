ce8f35e0576e5fe6f60848d28eef6f44a2a34ae7
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-12 09:30:09 +0100

    fixed indentation

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2).map(Number);
-function findSecondLargest(arr) {
+function findSecondLargest (arr) {
   if (arr.length <= 1) {
     return 0;
   }
