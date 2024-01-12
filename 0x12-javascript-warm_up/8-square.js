357854f4e6f42eecd24c0338ae59da2a586c8c93
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-12 01:44:22 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,15 +5,17 @@ const args = process.argv;
 if (args[2]) {
   const size = parseInt(args[2]);
 
-if (!isNaN(size) && size > 0) {
-    for (let i = 0; i < size; i++) {
-      let row = '';
-      for (let j = 0; j < size; j++) {
-        row += 'X';
+  if (!isNaN(size) && size > 0) {
+      for (let i = 0; i < size; i++) {
+        let row = '';
+        for (let j = 0; j < size; j++) {
+          row += 'X';
+        }
+        console.log(row);
       }
-      console.log(row);
+    } else {
+      console.log('Missing size');
     }
-  }
 } else {
   console.log('Missing size');
 }
