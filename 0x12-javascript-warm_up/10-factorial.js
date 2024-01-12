f669bd49423ffdc6cc3c6d010b24d6f5370a29e6
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-12 09:22:18 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,11 @@
 
 const process = require('process');
 const arg = process.argv;
-const n = 5;
 function factorial (n) {
   if (n === 0) {
     return 1;
   } else {
-    return n * factorial ( n - 1 );
+    return n * factorial(n - 1);
   }
 }
 if (parseInt(arg[2])) {
