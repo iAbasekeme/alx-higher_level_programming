e638e2af72e68a8d0e81ac057fe43faeb7c07e00
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-13 11:19:25 +0100

    Create 8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+// #!/usr/bin/node
+
+exports.esrever = function (list) {
+  if (list) {
+    const storage = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+      storage.push(list[i]);
+    }
+    return storage;
+  } else {
+    return;
+  }
+}
