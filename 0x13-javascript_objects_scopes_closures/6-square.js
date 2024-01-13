7f12d3149ec30ffd1e6ca0cc1ad5582b1b3e9218
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-13 04:30:13 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ class Square extends Square2 {
     this.size = size;
   }
 
-  charPrint(c = 'X') {
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
