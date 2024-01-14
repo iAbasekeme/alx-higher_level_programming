a3575fa3cc83a50ccb1f2ef8c27fc2f4cc94389d
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-14 15:43:04 +0100

    Create 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa """
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    try:
+        cur.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except Exception:
+        pass
