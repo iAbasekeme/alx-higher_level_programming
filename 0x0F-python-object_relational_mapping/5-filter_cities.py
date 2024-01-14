46ca6cb982aa816abfefaef28ef16dcbb86060f5
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-14 23:50:38 +0100

    Create 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script that lists all cities from
+the database hbtn_0e_0_usa using argv
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    try:
+        query = "SELECT * cities.name \
+            FROM cities \
+            LEFT JOIN states \
+            ON states.id = cities.state_id"
+        cur.execute(query)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except Exception:
+        pass
