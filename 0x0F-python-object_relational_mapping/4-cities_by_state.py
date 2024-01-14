905a44dca8b0f4caf647a86ef9de97d769eb1b9d
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-14 22:57:39 +0100

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,10 @@ if __name__ == "__main__":
         host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     try:
-        query = "SELECT * FROM cities"
+        query = "SELECT cities.id, cities.name, states.name \
+            FROM cities \
+            INNER JOIN states \
+            ON cities.state_id = states.id"
         cur.execute(query)
         rows = cur.fetchall()
         for row in rows:
