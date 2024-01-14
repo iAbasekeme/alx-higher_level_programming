0fdc8187c52ab3516cbc50d8affb31a1b5791a2c
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-14 15:14:52 +0100

    Create 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+"""
+A script that lists all states from the database hbtn_0e_0_usa using argv:
+"""
+
+from sys import argv
+db = MySQLdb.connect(
+    host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+
+cur = db.cursor()
+try:
+  cur.execute("SELECT * FROM states ORDER BY id ASC")
+  rows = cur.fetchall()
+  for row in rows:
+      print(row)
+  cur.close()
+  db.close()
+except Exception:
+    pass
+
+if __name__ == "__main__":
+    main()
