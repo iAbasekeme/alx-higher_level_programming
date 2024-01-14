cff578b56633461c05f9471d1c1672b9706ded9d
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-14 22:16:27 +0100

    Create 4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A script that lists all cities from
+the database hbtn_0e_0_usa using argv
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    try:
+        query = "SELECT * FROM cities"
+        cur.execute(query)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except Exception:
+        pass
