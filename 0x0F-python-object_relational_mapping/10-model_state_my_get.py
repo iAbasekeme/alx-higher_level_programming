eb5b4485bf59ac613a5f4d9047055bb64a182115
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-18 01:02:26 +0100

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter_by(State.name == argv[4])
+    state = session.query(State).filter(State.name == argv[4]).first()
     if state is None:
         print('Not found')
     else:
