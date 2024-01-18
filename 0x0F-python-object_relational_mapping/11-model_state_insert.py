5e634c25748e0b43b73ddbee1cb916e5e49b5f3b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-18 09:32:38 +0100

    Create 11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that adds the
+State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name='Louisiana')
+    session.add(state)
+
+    session.commit()
+    added_state = session.quey(State).filter(State.name == 'Louisiana').first()
+    if added_state:
+        print(f"{added_state.id}")
