c882938d46a025eadc701d9e175d52bc878e9b88
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-21 11:01:58 +0100

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,7 +9,7 @@ from sqlalchemy import *
 
 class City(Base):
     """
-    A city class
+    A city class that will define the cities table
     """
     __tablename__ = 'cities'
     id = Column(Integer, Primary_Key=True, autoincrement=True)
