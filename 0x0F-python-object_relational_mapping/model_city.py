44186e2eb1418c0c07bc149d90b3339c95937ca5
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-21 11:04:59 +0100

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Python file similar to model_state.py named model_city.py \
-that contains the class definition of a City
+module city
 """
 from model_state import Base
 from sqlalchemy import *
@@ -14,4 +13,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, Primary_Key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(states.id), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
