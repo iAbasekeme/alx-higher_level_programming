1ac41bf3ff7ba7db4bf4f88c06d630e29414b016
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-25 11:38:23 +0100

    fixed lengthy comments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, 
-# sends a request to that URL, and displays the size of the body of the response
+# This script takes in a URL, ans send a request
 curl -s "${1}" | wc -c
