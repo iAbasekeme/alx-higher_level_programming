648ef42b919291dabca74274cb3b86c0b29b404e
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-26 16:49:44 +0100

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         x_request_id = response.getheader('X-Request-Id')
         if x_request_id:
-            printf(x_request_id)
+            print(x_request_id)
