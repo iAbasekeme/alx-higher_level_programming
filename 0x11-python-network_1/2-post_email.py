ac89a3bbc87a54b8d2acd342afdcf80f8a3b8f72
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-26 18:06:49 +0100

    Create 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a script that sends a POST request to the passed URL \
+        with email as a parameter
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = {'email': email}
+
+    # Encode the data for a POST request
+    data = urllib.parse.urlencode(data)
+    data = data.encode('utf-8')  # Data should be bytes
+    with urllib.request.urlopen(url, data) as response:
+        result = response.read()
+        print(f"Your email is: {format(result.decode('utf-8'))}")
