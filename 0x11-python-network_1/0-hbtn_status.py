ee0746741d4e82440967040699de65e355e3e147
iAbasekeme <abasiekemeetim20@gmail.com>
2024-01-26 06:25:38 -0800

    fixed incomplete output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     import urllib.request
     with urllib.request.\
             urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read().decode('utf-8')
+        html = response.read()
 
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html))
