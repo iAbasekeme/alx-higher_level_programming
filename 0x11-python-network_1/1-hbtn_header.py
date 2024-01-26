26aa01eee9858e347b821e296a4d713505850196
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-26 16:50:46 +0100

    removed wrong command-line print

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,6 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    print(sys.argv[1])
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         x_request_id = response.getheader('X-Request-Id')
