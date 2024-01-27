dbfb01a384ccae4a0d3a23a3a74c075850b3d7a9
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-27 07:09:01 +0100

    fixed errors

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,11 +10,10 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {'email': email}
-
-    # Encode the data for a POST request
-    data = urllib.parse.urlencode(data)
-    data = data.encode('utf-8')  # Data should be bytes
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        result = response.read()
-        print(f"Your email is: {result.decode('utf-8')}")
+    if len(argv) > 2:
+        # Encode the data for a POST request
+        data = urllib.parse.urlencode(data).encode('utf-8')
+        req = request.Request(url, data)
+        with urllib.request.urlopen(req) as response:
+            result = response.read()
+            print(f"Your email is: {result.decode('utf-8')}")
