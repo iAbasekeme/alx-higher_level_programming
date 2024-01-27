b4f56e94cabcfd2d1100e935d3732d6a1a3c7df9
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-27 11:11:30 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,6 +2,8 @@
 """A script that sends a request and displays the X-Request-Id
 """
 import request
+from sys import argv
 
 if __name__ == "__main__":
-    r = request
+    r = request.get(argv[1])
+    print(r.headers['X-Request-Id'])
