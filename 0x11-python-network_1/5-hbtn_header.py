15a774116f3a9b4e609881f88ec58e8b94594ed1
iAbasekeme <abasiekemeetim20@gmail.com>
2024-01-27 02:13:19 -0800

    an executable file

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """A script that sends a request and displays the X-Request-Id
 """
-import request
+import requests
 from sys import argv
 
 if __name__ == "__main__":
-    r = request.get(argv[1])
+    r = requests.get(argv[1])
     print(r.headers['X-Request-Id'])
