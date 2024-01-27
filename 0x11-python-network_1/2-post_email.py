f183fd8566d536d8b789ddbe84f5268d759ad19b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-27 07:10:02 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {'email': email}
-    if len(argv) > 2:
+    if len(sys.argv) > 2:
         # Encode the data for a POST request
         data = urllib.parse.urlencode(data).encode('utf-8')
         req = request.Request(url, data)
