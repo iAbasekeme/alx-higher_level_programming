d87c0c3a64d1f2f9cbc19d3b22657a21746f0c86
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-27 07:24:05 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,12 +6,13 @@ from urllib import parse, request
 from sys import argv
 
 if __name__ == "__main__":
-    url = argv[1]
-    email = argv[2]
-    value = {'email': email}
     if len(argv) > 2:
+        url = argv[1]
+        email = argv[2]
+        value = {'email': email}
+
         # Encode the data for a POST request
-        data = parse.urlencode(value).encode('utf-8')
+        data = parse.urlencode(value).encode('ascii')
         req = request.Request(url, data)
         with request.urlopen(req) as response:
             result = response.read()
