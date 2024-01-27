f4de4134df18a3141a666b8aa441952e6a7aec5f
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-27 07:47:58 +0100

    Create 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that handles httpError
+"""
+
+from urllib import request, error
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except error.URLError as e:
+        print(f"Error code: {e}")
