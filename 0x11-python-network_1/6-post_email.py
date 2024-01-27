9dc5d60a9ca3c41e29eb97b4aba459299de93867
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-27 12:30:36 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,4 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     data = argv[2]
-
-    r = requests.post(url, data)
-    print(r.text)
+    print(requests.post(url, data).text)
