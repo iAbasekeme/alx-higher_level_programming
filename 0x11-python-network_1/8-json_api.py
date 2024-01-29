bf38ee2043a3569fe28212dd74ec7c1710f113c1
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-29 01:33:31 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,12 +7,15 @@ from sys import argv
 if __name__ == "__main__":
     q = argv[1] if len(argv) > 1 else ""
     data = {'q': q}
-    requests.post('http://0.0.0.0:5000/search_user', data=data)
+    res = requests.post('http://0.0.0.0:5000/search_user', data=data)
 
-    json_data = response.json()
-    if json_data:
-        user_id = json_data.get('id', 'N/A')
-        user_name = json_data.get('name', 'N/A')
-        print("[{}] {}".format(user_id, user_name))
-    else:
-        print("No result")
+    try:
+        json_data = res.json()
+        if json_data:
+            user_id = json_data.get('id', 'N/A')
+            user_name = json_data.get('name', 'N/A')
+            print("[{}] {}".format(user_id, user_name))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
