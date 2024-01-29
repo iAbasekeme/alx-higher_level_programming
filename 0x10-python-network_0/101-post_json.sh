d1e058604f5577d9cfe65ebb41ddd94b5e20cfa0
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-29 04:02:22 +0100

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument
-echo "$(curl -s -X POST -H "Content-Type: application/json" --data @"$2" "$1")"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
