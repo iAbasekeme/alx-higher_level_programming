5b8234a7b08c73f02d8e08b2635647ea80cc69f1
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-29 01:35:53 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,8 +7,8 @@ from sys import argv
 if __name__ == "__main__":
     q = argv[1] if len(argv) > 1 else ""
     data = {'q': q}
-    res = requests.post('http://0.0.0.0:5000/search_user', data=data)
 
+    res = requests.post('http://0.0.0.0:5000/search_user', data=data)
     try:
         json_data = res.json()
         if json_data:
