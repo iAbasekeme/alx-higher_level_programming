32d0a8071994c40003fc691e3c951736f2da6dcc
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-01-31 01:13:26 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-
-const { list } = require('./100-data')
-const roots = list.map((value, index) => value * index);
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(roots);
+console.log(list.map((item, index) => item * index));
