39e694c8b879813e4b966e328332bd04207f4ef1
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-21 11:43:26 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,10 @@ const request = require('request');
 const arg = process.argv[2];
 
 const url = "https://swapi-api.alx-tools.com/api/films/" + arg;
-request(url, (error, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const data = JSON.parse(body);
-    console.log(data.title);
+    console.log(JSON.parse(body).title);
   }
 });
