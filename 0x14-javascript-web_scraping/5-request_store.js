78d4057e01591df8d27f6857958e183ea9159050
iAbasekeme <abasiekemeetim20@gmail.com>
2024-02-21 17:53:23 +0100

    chnaged file mode

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const arg = process.argv[2];
 const fs = require('fs');
-const file = process.argv[3]
-const page = process.argv[2]
+const file = process.argv[3];
+const page = process.argv[2];
 
 request(page, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    console.log(response.body);
+    const data = response.body;
+    fs.writeFile(file, data, 'utf-8', (err) => {
+	if (err) {
+		console.error(err);
+	}
+    })
   }
 })
