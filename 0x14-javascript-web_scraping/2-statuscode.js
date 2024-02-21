bf6ec406994d7c0148687ec6755fab7f21e09907
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-21 01:51:04 +0100

    fixed wrong indentation

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const request = require('request')
-const arg = process.argv[2]
+const request = require('request');
+const arg = process.argv[2];
 
 request(arg, (error, response, body) => {
   if (error) {
