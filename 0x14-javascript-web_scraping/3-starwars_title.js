56707f49a23e113ba5cf699485760acee0184613
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-21 11:45:01 +0100

    fixed semistandard errors

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const arg = process.argv[2];
 
-const url = "https://swapi-api.alx-tools.com/api/films/" + arg;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + arg;
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
