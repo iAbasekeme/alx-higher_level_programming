5b2c2e32f6e65df7f09794b079c591785ef1101e
iAbasekeme <abasiekemeetim20@gmail.com>
2024-02-21 19:56:30 +0100

    exe

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,5 +7,12 @@ request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   }
-  console.log(response.body);
+  const data = JSON.parse(body);
+  emptyObject = {}
+  for (let dat = 0; dat < data.length; dat++) {
+	  if (data[dat].completed) {
+		  emptyObject[data[dat].userId] = (emptyObject[data[dat].userId] || 0) + 1;
+	  }
+  }
+  console.log(emptyObject);
 })
