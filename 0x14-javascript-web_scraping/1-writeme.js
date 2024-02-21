c7e141bbb71d97c9c17b0f884451f08d14ef3f4b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-21 01:20:52 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const { error } = require('console')
 const fs = require('fs')
 const arg = process.argv[2]
-const string_path = process.argv[3]
+const stringPath = process.argv[3]
 
-fs.writeFile(arg, string_path, 'utf-8', (err) => {
-  if(err) {
+fs.writeFile(arg, stringPath, 'utf-8', (err) => {
+  if (err) {
     return console.error(err)
   }
 })
