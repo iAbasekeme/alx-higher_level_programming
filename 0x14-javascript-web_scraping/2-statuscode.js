72d794141234909d18b57ebc5c76ddd4b0e5f2db
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-21 01:54:22 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ request(arg, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    console.log('code: '+ response.statusCode);
+    console.log('code: ' + response.statusCode);
   }
 });
