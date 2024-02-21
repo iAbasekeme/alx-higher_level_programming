e4532689f9326cf1a5750a6fe9b1b055e15a2d5e
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-21 13:01:41 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const arg = process.argv[2];
-const characterId = '18';
+const characterId = 18
 
 request(arg, (error, response, body) => {
   if (error) {
@@ -9,11 +9,11 @@ request(arg, (error, response, body) => {
   } else {
     const data = JSON.parse(body).results;
     let count = 0;
-    data.forEach((film) => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+    for (let res = 0; res < data; res++) {
+      if (data[res].characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
         count++;
       }
-    });
+    }
     console.log(count);
   }
 });
