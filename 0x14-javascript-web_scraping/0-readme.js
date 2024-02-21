fa0a239947979dae4815ac9269e032d903067234
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-21 01:37:53 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const arg = process.argv[2]
 // Synchronously read the file
 fs.readFile(arg, 'utf-8', (err, data) => {
   if (err) {
-    return console.error(err)
+    console.error(err);
   }
-  console.log(data)
+  console.log(data);
 })
