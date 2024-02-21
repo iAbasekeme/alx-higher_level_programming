7fb19ab31b7aa0d02ca989a279a2406b19210189
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-21 01:49:35 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs')
-const arg = process.argv[2]
+const fs = require('fs');
+const arg = process.argv[2];
 
 // Synchronously read the file
 fs.readFile(arg, 'utf-8', (err, data) => {
