f3fa23afa8c248764ed68ca958105970ec4082a1
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-29 14:24:09 +0100

    fixed wrong attribute

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-  $.$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
-    $('UL#list_movies').prepend(data.title);
+  $.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    $.each(data.results, function (index, movie) {
+       $('UL#list_movies').prepend('<li>' + movie.title + '</li>');
+      });
     });
 });
